bce2f94d1f7ce759fbc5c157528db0ae31fd7b3f
Leslie Mwangi <luciannahphoi@gmail.com>
2024-05-24 23:42:43 +0300

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    '''prints fibonacci sequence of given length'''
     if length == 0:
         print('[]')
     elif length == 1:
         print('[0]')
+    elif length == 2:
+        print('[0, 1]')
     else:
         fib = [0, 1]
         for i in range(2, length):
             fib.append(fib[i - 1] + fib[i - 2])
         print('[' + ', '.join(map(str, fib)) + ']')
+  
